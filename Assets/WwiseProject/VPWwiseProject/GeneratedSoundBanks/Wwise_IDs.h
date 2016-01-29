5bf17d6c0670de7f647b67f1ab551ea347b8b4d4
Pablo Schwilden Diaz <peschwildendiaz@hotmail.com>
2016-01-29 23:32:23 +0100

    Wwise new test

---
+++ Assets/WwiseProject/VPWwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID OUTDOORAMB_LOOP = 1673655846U;
+        static const AkUniqueID PEOPLEPRAY = 3573189912U;
+        static const AkUniqueID PEOPLESAD = 2623792584U;
+        static const AkUniqueID PEOPLEUH = 3293427131U;
+        static const AkUniqueID PEOPLEWALK = 1023995335U;
+        static const AkUniqueID UIDROP = 1389954868U;
+        static const AkUniqueID UIGRAB = 3379585205U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID VPMAINSOUNDBANK = 2676280715U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID OBJECTS = 1695690031U;
+        static const AkUniqueID PEOPLE = 1417401084U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID ROOMREV = 694004599U;
+    } // namespace AUX_BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
