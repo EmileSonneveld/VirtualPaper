f5e3dc1897f8f792dff6eb2afeeb3b37ede8d9f7
Thomas VB <van.bouwel.thomas@gmail.com>
2016-01-30 06:33:24 +0100

    interaction working - light turns on/off

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour {
+
+	public Light[] m_Lights;
+	public bool m_LightOn = true;
+	public Renderer m_LampRenderer;
+	public Material m_LightOnMat;
+	public Material m_LightOffMat;
+	
+	public void OnClick(){
+		m_LightOn = !m_LightOn;
+		foreach (Light L in m_Lights) {
+			L.enabled = m_LightOn;
+		}
+		if (m_LightOn) {
+			Material[] mats;
+			mats = m_LampRenderer.materials;
+			mats [5] = m_LightOnMat;
+			m_LampRenderer.materials = mats;
+		} else {
+			Material[] mats;
+			mats = m_LampRenderer.materials;
+			mats [5] = m_LightOffMat;
+			m_LampRenderer.materials = mats;
+		}
+	}
+}
