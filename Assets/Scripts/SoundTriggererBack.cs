2e6fecd616de893662648d3fd30dab5be905ec41
Thomas VB <van.bouwel.thomas@gmail.com>
2016-02-14 14:22:44 +0100

    Deleted virtualpaper recursive folder

---
+++ Assets/Scripts/SoundTriggererBack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTriggererBack : MonoBehaviour {
+
+	public void ChopSound()
+	{
+	}
+	public void ThrowSound(){
+	}
+	public void SacrificeSound(){
+	}
+	public void Footstep(){
+	}
+
+}
+
